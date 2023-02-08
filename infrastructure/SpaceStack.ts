33c166de1c46e660c812d2e23ad7b35e906e3451
Elvis Yung <elvisckyung@gmail.com>
2023-02-08 16:57:38 +0000

    Securing APIs with AWS Cognito

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,18 +1,20 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import {
-  Code,
-  Function as LambdaFunction,
-  Runtime,
-} from 'aws-cdk-lib/aws-lambda';
 import { join } from 'path';
-import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import {
+  AuthorizationType,
+  LambdaIntegration,
+  MethodOptions,
+  RestApi,
+} from 'aws-cdk-lib/aws-apigateway';
 import { GenericTable } from './GenericTable';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { AuthorizerWrapper } from './auth/AuthorizerWrapper';
 
 export class SpaceStack extends Stack {
   private api = new RestApi(this, 'SpaceApi');
+  private authorizer: AuthorizerWrapper;
   private spacesTable = new GenericTable(this, {
     tableName: 'SpacesTable',
     primaryKey: 'spaceId',
@@ -26,6 +28,8 @@ export class SpaceStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
+    this.authorizer = new AuthorizerWrapper(this, this.api);
+
     const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
       entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
       handler: 'handler',
@@ -35,10 +39,21 @@ export class SpaceStack extends Stack {
     s3ListPolicy.addResources('*');
     helloLambdaNodeJs.addToRolePolicy(s3ListPolicy);
 
+    const optionsWithAuthorizer: MethodOptions = {
+      authorizationType: AuthorizationType.COGNITO,
+      authorizer: {
+        authorizerId: this.authorizer.authorizer.authorizerId,
+      },
+    };
+
     // Hello Api Lambda integration:
     const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs);
     const helloLambdaResource = this.api.root.addResource('hello');
-    helloLambdaResource.addMethod('GET', helloLambdaIntegration);
+    helloLambdaResource.addMethod(
+      'GET',
+      helloLambdaIntegration,
+      optionsWithAuthorizer
+    );
 
     // Spaces API Integration
     const spaceResource = this.api.root.addResource('spaces');
---
+++ test/Auth/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  REGION: 'eu-west-1',
+  USER_POOL_ID: 'eu-west-1_2WmDY7X2t',
+  APP_CILENT_ID: '15qq8520m0b4npcgg0fa83441g',
+  TEST_USER_NAME: 'elvisyung2',
+  TEST_USER_PASSWORD: 'a3MPQqd&',
+};
