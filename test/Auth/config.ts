960b6dd86c44d53cbec86af7ece8d7812662bb6b
Elvis Yung <elvisckyung@gmail.com>
2023-02-08 21:40:47 +0000

    AWS Cognito Identity Pool Implementation

--- test/Auth/config.ts
+++ test/Auth/config.ts
@@ -2,6 +2,7 @@ export const config = {
   REGION: 'eu-west-1',
   USER_POOL_ID: 'eu-west-1_2WmDY7X2t',
   APP_CILENT_ID: '15qq8520m0b4npcgg0fa83441g',
+  IDENTITY_POOL_ID: 'eu-west-1:5ad6cbec-7836-4623-955e-7f4052a37809',
   TEST_USER_NAME: 'elvisyung2',
   TEST_USER_PASSWORD: 'a3MPQqd&',
 };
