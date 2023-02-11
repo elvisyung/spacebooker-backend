af0950e2e13c70e6770db3dc383df302bc96bba6
Elvis Yung <elvisckyung@gmail.com>
2023-02-11 17:35:26 +0000

    S3 CloudFront Deployment

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -12,6 +12,7 @@ import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { AuthorizerWrapper } from './auth/AuthorizerWrapper';
 import { Bucket, HttpMethods } from 'aws-cdk-lib/aws-s3';
+import { WebAppDeployment } from './WebAppDeployment';
 
 export class SpaceStack extends Stack {
   private api = new RestApi(this, 'SpaceApi');
@@ -38,6 +39,7 @@ export class SpaceStack extends Stack {
       this.api,
       this.spacesPhotosBucket.bucketArn + '/*'
     );
+    new WebAppDeployment(this, this.suffix);
 
     const optionsWithAuthorizer: MethodOptions = {
       authorizationType: AuthorizationType.COGNITO,
